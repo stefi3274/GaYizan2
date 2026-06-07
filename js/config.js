234315e2a25839df6eddb02436673a1ea124dac3
Stevenson Mervil <stefi3274@gmail.com>
2026-06-07 20:19:30 +0200

    Fix: S en var pour acces global

--- js/config.js
+++ js/config.js
@@ -1,14 +1,14 @@
 // ════════════════════════════════
 // SUPABASE CONFIG
 // ════════════════════════════════
-const SUPABASE_URL = 'https://bqfksgfzmiwgwrcgpkln.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxZmtzZ2Z6bWl3Z3dyY2dwa2xuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMTQxMzIsImV4cCI6MjA5NDc5MDEzMn0.4HStTml7qtLm879-S-sbzOuYEKlVNS6ET3h-BmIJTQQ';
-const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+Var SUPABASE_URL = 'https://bqfksgfzmiwgwrcgpkln.supabase.co';
+Var SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxZmtzZ2Z6bWl3Z3dyY2dwa2xuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMTQxMzIsImV4cCI6MjA5NDc5MDEzMn0.4HStTml7qtLm879-S-sbzOuYEKlVNS6ET3h-BmIJTQQ';
+Var sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // ════════════════════════════════
 // STATE GLOBAL
 // ════════════════════════════════
-const S = {
+Var S = {
   page: 'home', prev: null,
   products: [],
   myOrders: [],
